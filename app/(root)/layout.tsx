b807472de76298561761b5c396daff814d265d29
victor <victorgomesdacosta18@gmail.com>
2024-09-22 13:14:42 -0300

    create mobile nav component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,24 @@
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const loggedIn = { firstName : 'Victor', lastName: 'Gomes'};
+
   return (
     <main className="flex h-screen w-full font-inter">
-        SIDEBAR
+        <Sidebar user={loggedIn} />
+
+        <div className=" flex flex-col size-full">
+          <div className="root-layout">
+            <Image  src="/icons/logo.svg" width={30} height={30} alt="menu icon" />
+          </div>
+        </div>
+
         {children}
     </main>
   );
