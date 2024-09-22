1e8ba9733234e63b7b2b0ffae46fbfc1db1a480a
victor <victorgomesdacosta18@gmail.com>
2024-09-22 18:08:11 -0300

    create pages transfer banks and transactions history

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -23,19 +23,20 @@ const BankCard = ({ account, userName, showBalance = true}: CreditCardProps) => 
                             {userName}
                         </h1>
                         <h2 className="text-12 font-semibold text-white">
-                            ** / **
+                        &#9679; &#9679; / &#9679; &#9679;
                         </h2>
                     </div>
                     <p className="text-14 font-semibold tracking-[1.1px] text-white">
-                        **** **** **** <span className="text-16">${account.mask}</span>
+                    &#9679;&#9679;&#9679;&#9679; &#9679;&#9679;&#9679;&#9679; &#9679;&#9679;&#9679;&#9679; <span className="text-16">1234</span>
                     </p>
                 </article>
             </div>
 
             <div className="bank-card_icon">
                 <Image src="/icons/Paypass.svg" width={20} height={20} alt="pay" />
-                <Image src="/icons/mastercard.svg" width={45} height={32} alt="mastercard" />
+                <Image src="/icons/mastercard.svg" width={45} height={32} alt="mastercard" className="ml-5" />
             </div>
+            <Image src="/icons/lines.png" width={316} height={190} alt="lines" className="absolute top-0 left-0" />
         </Link>
     </div>
   )
