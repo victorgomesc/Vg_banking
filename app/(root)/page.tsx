e4ca54017c2b4a53caba34668dca401e00aef9d3
victor <victorgomesdacosta18@gmail.com>
2024-09-21 23:11:15 -0300

    feat: create home components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,23 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+    const loggedIn = { firstName: 'Victor' };
+
   return (
-    <div>Home</div>
+    <section className="home">
+        <div className="home-content">
+            <HeaderBox type="greeting" title="Welcome," user={ loggedIn?.firstName || 'Guest'} 
+                subtext="Acess and manage your account and trasanctions efciently." />
+            
+            <TotalBalanceBox 
+                accounts={[]}
+                totalBanks={1}
+                totalCurrentBalance={1250.35}
+             />
+        </div>
+    </section>
   )
 }
 
