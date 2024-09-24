11a32227b5d4177a5a22e30370a617e4a4cf071b
victor <victorgomesdacosta18@gmail.com>
2024-09-24 16:17:10 -0300

    fix: error an import Input

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -16,7 +16,7 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+import { Input } from "./ui/input"
 import CustomInput from './CustomInput'
 import { authFormSchema } from '@/lib/utils'
 
