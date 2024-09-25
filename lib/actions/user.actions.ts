40ef3502ef44ce666a19f7f4207438bc149f7720
victor <victorgomesdacosta18@gmail.com>
2024-09-24 22:13:08 -0300

    feat: appwrite create conponentes

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,5 +1,10 @@
 "use server";
 
+import { ID } from "node-appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
+import { cookies } from "next/headers";
+import { parseStringify } from "../utils";
+
 export const signIn = async () => {
     try {
         // mutation database make fetch
@@ -9,9 +14,36 @@ export const signIn = async () => {
 }
 
 export const signUp = async (userData: SignUpParams) => {
+    const { email, password, firstName, lastName } = userData;
     try {
-        //create user account
+        const { account } = await createAdminClient();
+
+  const newUserAccount = await account.create(ID.unique(), 
+    email, 
+    password, 
+    `${firstName} ${lastName}`
+);
+  const session = await account.createEmailPasswordSession(email, password);
+
+  cookies().set("appwrite-session", session.secret, {
+    path: "/",
+    httpOnly: true,
+    sameSite: "strict",
+    secure: true,
+  });
+    return parseStringify(newUserAccount);
     } catch (error) {
         console.log('Error', error);
     }
 }
+
+
+export async function getLoggedInUser() {
+    try {
+      const { account } = await createSessionClient();
+      return await account.get();
+    } catch (error) {
+      return null;
+    }
+  }
+  
