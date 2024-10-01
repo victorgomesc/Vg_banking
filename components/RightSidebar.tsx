ab1b8545eab56cef42812b9908c7caa5d7676440
victor <victorgomesdacosta18@gmail.com>
2024-10-01 02:07:15 -0300

    refactor: refactoring yours components

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -36,11 +36,11 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
             {banks?.length > 0 && (
                 <div className="relative flex flex-1 flex-col items-center justify-center gap-5">
                     <div className="relative z-10">
-                        <BankCard key={banks[0].$id} account={banks[0]} userName={`${user.firstName} ${user.lastName}`} showBalance={false} />
+                        <BankCard key={banks[0].$id} account={banks[0]} userName={user.name} showBalance={false} />
                     </div>
                     {banks[1] && (
                       <div className="absolute right-0 top-8 z-0 w-[90%]">
-                        <BankCard key={banks[1].$id} account={banks[1]} userName={`${user.firstName} ${user.lastName}`} showBalance={false} />
+                        <BankCard key={banks[1].$id} account={banks[1]} userName={user.name} showBalance={false} />
                       </div>  
                     )}
                 </div>
