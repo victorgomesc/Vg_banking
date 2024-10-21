e8386497b2a1a703e08c0148d847996220c149df
victor <victorgomesdacosta18@gmail.com>
2024-10-21 17:31:53 -0300

    fix: to correc error in user.actions and plaid

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -15,11 +15,36 @@ const {
   APPWRITE_BANK_COLLECTION_ID: BANK_COLLECTION_ID,
 } = process.env;
 
+export const getUserInfo = async ({ userId}: getUserInfoProps) => {
+  try {
+    const { database } = await createAdminClient();
+
+    const user = await database.listDocuments(
+      DATABASE_ID!,
+      USER_COLLECTION_ID!,
+      [Query.equal('userId', [userId])]
+    )
+    return parseStringify(user.documents[0])
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 export const signIn = async ({ email, password }: signInProps) => {
     try {
         const { account } = await createAdminClient();
-        const response = await account.createEmailPasswordSession(email, password);
-        return parseStringify(response)
+        const session = await account.createEmailPasswordSession(email, password);
+
+        cookies().set("appwrite-session", session.secret, {
+          path: "/",
+          httpOnly: true,
+          sameSite: "strict",
+          secure: true,
+        });
+
+        const user =  await getUserInfo({ userId: session.userId })
+
+        return parseStringify(user)
     } catch (error) {
         console.log('Error', error);
     }
@@ -211,6 +236,21 @@ export async function getLoggedInUser() {
         [Query.equal('userId', [userId])]
       )
       return parseStringify(banks.documents)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  export const getBank = async ({ documentId }: getBanksProps) => {
+    try {
+      const { database } = await createAdminClient();
+
+      const bank = await database.listDocuments(
+        DATABASE_ID!,
+        BANK_COLLECTION_ID!,
+        [Query.equal('$id', [documentId])]
+      )
+      return parseStringify(bank.documents[0])
     } catch (error) {
       console.log(error)
     }
