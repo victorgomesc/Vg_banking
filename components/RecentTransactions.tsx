e97edf389ff10e6bc7890927af40b8539e43dc81
victor <victorgomesdacosta18@gmail.com>
2024-10-21 22:17:11 -0300

    feat: implement recent transactions and ui tabs

--- components/RecentTransactions.tsx
+++ components/RecentTransactions.tsx
@@ -1,8 +1,19 @@
+import Link from 'next/link'
 import React from 'react'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 const RecentTransactions = ({ accounts, transactions = [], appwriteItemId, page = 1 }: RecentTransactionsProps) => {
   return (
-    <div>RecentTransactions</div>
+    <section className="recent-transactions">
+        <header className="flex items-center justify-between">
+            <h2 className="recent-transactions-label">
+                Recent transactions
+            </h2>
+            <Link href={`/transaction-history/?id=${appwriteItemId}`} className="view-all-btn">
+                View all
+            </Link>
+        </header>
+    </section>
   )
 }
 
